84d7984fdeda520281e6965a1655f32784301554
funzter <funzter@mail.timesnake.de>
2024-01-04 01:01:21 +0100

    restructured modules

--- src/main/java/de/timesnake/extension/decoration/deco/DecoCmd.java
+++ src/main/java/de/timesnake/extension/decoration/deco/DecoCmd.java
@@ -11,9 +11,9 @@ import de.timesnake.basic.bukkit.util.chat.cmd.Sender;
 import de.timesnake.basic.bukkit.util.user.User;
 import de.timesnake.extension.decoration.armorstand.StandEditor;
 import de.timesnake.extension.decoration.heads.HeadsManager;
+import de.timesnake.library.chat.Code;
 import de.timesnake.library.commands.PluginCommand;
 import de.timesnake.library.commands.simple.Arguments;
-import de.timesnake.library.extension.util.chat.Code;
 
 public class DecoCmd implements CommandListener {
 
