8b9bbeb32048469eff161b7332e3b8d30f868f3a
funzter <funzter@mail.timesnake.de>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/extension/decoration/deco/Plugin.java
+++ src/main/java/de/timesnake/extension/decoration/deco/Plugin.java
@@ -4,17 +4,11 @@
 
 package de.timesnake.extension.decoration.deco;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin DECO = new Plugin("Deco", "EXD",
-      LogHelper.getLogger("Deco", Level.INFO));
+  public static final Plugin DECO = new Plugin("Deco", "EXD");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
